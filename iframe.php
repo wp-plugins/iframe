e3c5bb4000de791acbdb7784e2ee8353433376d5
webvitaly <webvitaly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-07 18:35:16 +0000

    Commit 1.1.0 version
    git-svn-id: https://plugins.svn.wordpress.org/iframe/trunk@382224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iframe.php
+++ iframe.php
@@ -4,15 +4,15 @@
 Plugin Name: Iframe
 Plugin URI: http://web-profile.com.ua/wordpress/plugins/iframe/
 Description: Plugin shows iframe with [iframe width="640" height="480" src="http://player.vimeo.com/video/3261363"] shortcode.
-Version: 1.0.0
+Version: 1.1.0
 Author: webvitaly
 Author Email: webvitaly(at)gmail.com
 Author URI: http://web-profile.com.ua/
 */
 
 
-if ( !function_exists( 'iframe_embed_sortcode' ) ) {
-	function iframe_embed_sortcode($atts, $content = null) {
+if ( !function_exists( 'iframe_embed_shortcode' ) ) {
+	function iframe_embed_shortcode($atts, $content = null) {
 		extract(shortcode_atts(array(
 			'width' => '640',
 			'height' => '480',
@@ -20,10 +20,11 @@ if ( !function_exists( 'iframe_embed_sortcode' ) ) {
 			'frameborder' => '0',
 			'scrolling' => 'no',
 			'marginheight' => '0',
-			'marginwidth' => '0'
+			'marginwidth' => '0',
+			'allowtransparency' => 'true'
 		), $atts));
-		return '<iframe width="'.$width.'" height="'.$height.'" frameborder="'.$frameborder.'" scrolling="'.$scrolling.'" marginheight="'.$marginheight.'" marginwidth="'.$marginwidth.'" src="'.$src.'" ></iframe>';
+		return '<iframe width="'.$width.'" height="'.$height.'" frameborder="'.$frameborder.'" scrolling="'.$scrolling.'" marginheight="'.$marginheight.'" marginwidth="'.$marginwidth.'" allowtransparency="'.$allowtransparency.'" src="'.$src.'"></iframe>';
 		// &amp;output=embed
 	}
-	add_shortcode('iframe', 'iframe_embed_sortcode');
+	add_shortcode('iframe', 'iframe_embed_shortcode');
 }
--- readme.txt
+++ readme.txt
@@ -5,8 +5,8 @@ Plugin URI: http://web-profile.com.ua/wordpress/plugins/iframe/
 Tags: iframe, embed
 Author URI: http://web-profile.com.ua/wordpress/
 Requires at least: 3.0
-Tested up to: 3.1.1
-Stable tag: 1.0.0
+Tested up to: 3.1.2
+Stable tag: 1.1.0
 
 You may embed iframe with [iframe width="640" height="480" src="http://vimeo.com/123"] shortcode.
 
@@ -25,10 +25,14 @@ So you can embed iframe code using this shortcode **`[iframe width="640" height=
 * scrolling - scrolling parameter of the iframe `[iframe scrolling="no" src="http://player.vimeo.com/video/3261363"]` (by default scrolling="no");
 * marginheight - marginheight parameter of the iframe `[iframe marginheight="0" src="http://player.vimeo.com/video/3261363"]` (by default marginheight="0");
 * marginwidth - marginwidth parameter of the iframe `[iframe marginwidth="0" src="http://player.vimeo.com/video/3261363"]` (by default marginwidth="0");
+* allowtransparency - allowing transparency of the iframe `[iframe allowtransparency="true" src="http://player.vimeo.com/video/3261363"]` (by default allowtransparency="true");
 
 [WordPress stuff](http://web-profile.com.ua/wordpress/)
 
 == Changelog ==
+
+= 1.1.0 =
+* Parameter allowtransparency added (thanks to kent);
 
 = 1.0.0 =
 * Initial release;
