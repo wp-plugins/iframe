409ecb74e68ff214a638a3f6b447663f8691beb0
webvitaly <webvitaly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-25 18:36:15 +0000

    1.5.0 version commit;
    git-svn-id: https://plugins.svn.wordpress.org/iframe/trunk@443315 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iframe.php
+++ iframe.php
@@ -1,16 +1,21 @@
 <?php
-
 /*
 Plugin Name: Iframe
 Plugin URI: http://web-profile.com.ua/wordpress/plugins/iframe/
 Description: Plugin shows iframe with [iframe width="100%" height="480" src="http://player.vimeo.com/video/3261363"] shortcode.
-Version: 1.4.0
+Version: 1.5.0
 Author: webvitaly
 Author Email: webvitaly(at)gmail.com
-Author URI: http://web-profile.com.ua/
+Author URI: http://web-profile.com.ua/wordpress/
 */
 
-if ( !function_exists( 'iframe_embed_shortcode' ) ) {
+if ( !function_exists( 'iframe_embed_shortcode' ) ) :
+
+	function iframe_enqueue_script() {
+		wp_enqueue_script( 'jquery' );
+	}
+	add_action('wp_enqueue_scripts', 'iframe_enqueue_script');
+				
 	function iframe_embed_shortcode($atts, $content = null) {
 		extract(shortcode_atts(array(
 			'width' => '100%',
@@ -33,12 +38,11 @@ if ( !function_exists( 'iframe_embed_shortcode' ) ) {
 		}
 		$return = '';
 		if( $same_height_as != '' ){
-			$return .= '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>'; // including jQuery 1.6.x
 			$return .= '
-				<script type="text/javascript">
-				jQuery(document).ready(function() {
-					var target_height = jQuery("'.$same_height_as.'").height();
-					jQuery("iframe.'.$class.'").height(target_height);
+				<script>
+				jQuery(document).ready(function($) {
+					var target_height = $("'.$same_height_as.'").height();
+					$("iframe.'.$class.'").height(target_height);
 				});
 				</script>
 			';
@@ -53,4 +57,5 @@ if ( !function_exists( 'iframe_embed_shortcode' ) ) {
 		return $return;
 	}
 	add_shortcode('iframe', 'iframe_embed_shortcode');
-}
+	
+endif;
